f2d266977c3e71ef0ff3802336ff1017cf166c29
Rye1606Zz <49356827+thanhsonopera@users.noreply.github.com>
2022-05-22 17:33:09 +0700

    Delete CS Plane directory

--- CS Plane/CheckCollision.h
+++
@@ -1,8 +0,0 @@
-#ifndef CheckCollision_h
-#define CheckCollision_h
-
-#include <SDL.h>
-
-bool CheckCollision(const SDL_Rect &a, const SDL_Rect &b);
-
-#endif
--- CS Plane/Value.h
+++
@@ -1,9 +0,0 @@
-#ifndef Value_h
-#define Value_h
-
-#include "LTexture.h"
-
-LTexture Bg;
-LTexture *gTextTexture = new LTexture[5];
-
-#endif
